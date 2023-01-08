a0291875e12dd8e3d08e2a64f80a7490337f31d9
priyanshu <priyanshushrama709@gmail.com>
2023-01-08 11:13:35 +0530

    add count comment number

--- client/src/components/Dashboard-components/ReplyComment.jsx
+++ client/src/components/Dashboard-components/ReplyComment.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 import SingleComment from './SingleComment'
 
 function ReplyComment({ commentList, bugId, refreshComments, parentCommentId }) {
 
+    const [childCommentNumber,setChildCommentNumber] = useState(0)
+
+    useEffect(() => {
+        let commentNum = 0;
+        commentList.map(cmt => {
+            if((cmt.respondTo && cmt.respondTo._id) === parentCommentId) {
+                commentNum++
+            }
+        })
+        setChildCommentNumber(commentNum)
+    }, [])
+
+    
 
 
     return (
         <div>
-            <button className='btn btn-primary'>View more comments</button>
+            <button className='btn btn-primary'>View {childCommentNumber} more comments</button>
             {
 
                 commentList && commentList.map(cmt => {
